79096f3c5972da5fcefe210502d610b08c4a0bb1
aidancuite <awc82@cornell.edu>
2024-05-27 11:40:28 -0400

    things

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+// Use GLFW for surfaces.
+#define GLFW_INCLUDE_NONE
+#include "3rdparty\glfw\deps\glad\gl.h"
+#include "3rdparty/glfw/include/GLFW/glfw3.h"
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Begin things." << std::endl;
+
+	if(!glfwInit()){
+		std::cerr << "Failed to init glfw" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	
+
+	glfwTerminate();
+	std::cout << "GLFW Terminated Succesfully" << std::endl;
 
 	return EXIT_SUCCESS;
 }
