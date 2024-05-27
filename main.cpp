924b996906386ff93358c3a9a192e706668d01cf
aidancuite <awc82@cornell.edu>
2024-05-27 14:16:54 -0400

    imgui test window

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #include "stdio.h"
 
 // Error callback for glfw.
@@ -30,20 +34,59 @@ int main() {
 		return EXIT_FAILURE;
 	}
 
-	//Create the glfw window. Require at least OpenGL 2.0
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+// Versioning logic for ImGui and opengl build things.
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+	//Create the glfw window.
 	GLFWwindow* window = glfwCreateWindow(640, 480, "DSRender", NULL, NULL);
 	if(!window){
 		std::cerr << "Failed to init glfwWindow" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
 	}
+	std::cout << "Initialized GLFW Window" << std::endl;
 
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1); //Set 1 cycle minimum swap window inorder to reduce wasted calls.
 	glfwSetKeyCallback(window, key_callback);
 
+	// Setup ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// OpenGL Integration
 	if(!gladLoadGL(glfwGetProcAddress)){
 		std::cerr << "Failed to init OpenGL" << std::endl;
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return EXIT_FAILURE;
 	}
 	std::cout << "Initialized OpenGL" << std::endl;
 
@@ -51,13 +94,47 @@ int main() {
 	double deltaTime = 0;
 	double upTime = 0;
 
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	bool first = true;
 	while(!glfwWindowShouldClose(window)){  
 		std::cout << "Delta T: " << deltaTime << "\n";
+
+		//ImGUI Debug Frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		//ImGui example frame.
+		static float f = 0.0f;
+        static int counter = 0;		
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            // ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            // ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button")) { // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		}                           
+                
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+		ImGui::Render(); // Render current ImGui frame to window. 
 		
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
+
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -66,6 +143,11 @@ int main() {
 		deltaTime = glfwGetTime() - upTime;
 		upTime = glfwGetTime();
 	}
+
+	// ImGui Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 	
 	//Free the window.
 	glfwDestroyWindow(window);
